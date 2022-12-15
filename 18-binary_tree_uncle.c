629e978de1110c2cc03074b20749e0c859cb0fd4
Michelle-chats <mtchatikobo@gmail.com>
2022-12-15 02:38:22 -0800

    submitting

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling
- * 
+ *
  * Return: pointer to the sibling node
  *         NULL if node is NULL
  *         NULL if the parent is NULL
